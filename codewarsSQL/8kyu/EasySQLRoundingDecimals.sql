b8578377c4b9b973598454c8ba256608105357e2
aleksLiss <darklisik.darklisik@yandex.ru>
2024-04-24 23:50:12 +0300

    'SqlBasic:RoundingDecimals' have done

---
+++ codewarsSQL/8kyu/EasySQLRoundingDecimals.sql
@@ -0,0 +1,19 @@
+/*
+DESCRIPTION:
+
+Given the following table 'decimals':
+
+** decimals table schema **
+
+    id
+    number1
+    number2
+
+Return a table with two columns (number1, number2), the value in number1 should be rounded down and the value in number2 should be rounded up.
+
+
+SOLUTION:
+*/
+
+
+SELECT FLOOR(number1) AS NUMBER1, CEILING(number2) AS NUMBER2 FROM decimals;
